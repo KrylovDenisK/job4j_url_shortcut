8e9aaaf0daf6252890e2326e401644384dcf2345
Krylov Denis <d.krylovk@gmail.com>
2021-11-16 16:20:39 +0700

    travic codecov

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+[![Build Status](https://app.travis-ci.com/KrylovDenisK/job4j_url_shortcut.svg?branch=main)](https://app.travis-ci.com/KrylovDenisK/job4j_url_shortcut)
+[![codecov](https://codecov.io/gh/KrylovDenisK/job4j_url_shortcut/branch/main/graph/badge.svg?token=HQGBZPWG7S)](https://codecov.io/gh/KrylovDenisK/job4j_url_shortcut)
+
 "# job4j_url_shortcut" 
--- src/main/java/ru/job4j/shortcut/config/Configurations.java
+++ src/main/java/ru/job4j/shortcut/config/Configurations.java
@@ -1,13 +1,12 @@
 package ru.job4j.shortcut.config;
 
-import liquibase.integration.spring.SpringLiquibase;
-import org.springframework.beans.factory.annotation.Qualifier;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.sql.DataSource;
+
 
 @Configuration
 public class Configurations {
